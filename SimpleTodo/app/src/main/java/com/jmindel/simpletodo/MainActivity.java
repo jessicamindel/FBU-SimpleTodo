d7197a2134c637c2553256a11743f8b551e1a01b
Jessie Mindel <mindel.jessie@gmail.com>
2019-06-25 15:54:35 -0700

    Finish basic implementation
    Includes adding, removing, and persistence.

--- SimpleTodo/app/src/main/java/com/jmindel/simpletodo/MainActivity.java
+++ SimpleTodo/app/src/main/java/com/jmindel/simpletodo/MainActivity.java
@@ -1,14 +1,88 @@
 package com.jmindel.simpletodo;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        readItems();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems = (ListView) findViewById(R.id.lvItems); // Just like DOM querying in JS!
+        lvItems.setAdapter(itemsAdapter);
+
+//        // Add sample items
+//        for (int i = 1; i <= 5; i++) {
+//            items.add("Item " + i);
+//        }
+
+        setupListViewListener();
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText); // LEARN: Why are we adding to itemsAdapter instead of items here, while we add to items for sample data?
+        etNewItem.setText("");
+        writeItems();
+        Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupListViewListener() {
+        Log.i("MainActivity", "Setting up listener on list view");
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i("MainActivity", "Item removed from list: " + i);
+                items.remove(i);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true; // LEARN: Prevent bubbling by consuming?
+            }
+        });
+    }
+
+    private File getDataFile() {
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    private void readItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading file", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing file", e);
+        }
     }
 }
